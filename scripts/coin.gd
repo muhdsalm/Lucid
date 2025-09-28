7c73598b9f6bd1ddb98cd1d7a3c2bf9746c65994
Muhammad Salman <muhdsalm44@gmail.com>
2025-09-28 15:20:08 +0500

    small tweaks

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -8,8 +8,9 @@ func _on_body_entered(body):
 	$CollisionShape2D.queue_free()
 	$AudioStreamPlayer.play()
 	Data.coins += 1
-	if Data.coins >= 39:
-		get_tree().change_scene_to_file("res://Title Screen.tscn")
+	if Data.coins >= 40:
+		Data.reset()
+		get_tree().change_scene_to_file("res://win screen.tscn")
 
 
 func _on_timer_timeout() -> void:
